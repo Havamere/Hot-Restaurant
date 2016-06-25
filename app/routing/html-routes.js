1815e471d130f091bd427a194b20881fd1beed49
Pinar Tokmak <ptokmak@hotmail.com>
2016-06-25 13:35:08 -0400

    Updated/Cleaned Up

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -2,7 +2,7 @@
 // Routes
 // =============================================================
 
-// Basic route that sends the user first to the AJAX Page
+// Basic route that sends the user first to the AJAX Page:
 app.get('/', function(req, res){	
 	res.sendFile(path.join(__routing + '/home.html'));
 })
