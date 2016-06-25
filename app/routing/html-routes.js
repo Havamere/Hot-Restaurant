ddb8aa0b497c7072d3251038b977ef06453e60af
Pinar Tokmak <ptokmak@hotmail.com>
2016-06-25 11:55:41 -0400

    Updated routes

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -1,20 +1,3 @@
-// Dependencies
-// =============================================================
-var express = require('express');
-var bodyParser = require('body-parser');
-var path = require('path');
-
-// Sets up the Express App
-// =============================================================
-var app = express();
-var PORT = 3000;
-
-// Sets up the Express app to handle data parsing 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.text());
-app.use(bodyParser.json({type:'application/vnd.api+json'}));
-
 
 // Routes
 // =============================================================
@@ -32,10 +15,3 @@ app.get('/add', function(req, res){
 	res.sendFile(path.join(__routing + '/table-logic.html'));
 })
 
-
-
-// Starts the server to begin listening 
-// =============================================================
-app.listen(PORT, function(){
-	console.log('App listening on PORT ' + PORT);
-})
